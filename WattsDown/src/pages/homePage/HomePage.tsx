701dc0c4f9d442f051458c5afee476ed2a312539
Xamalf <34105901+Xamalf@users.noreply.github.com>
2023-03-07 12:11:00 +0100

    css on homescreen

--- WattsDown/src/pages/homePage/HomePage.tsx
+++ WattsDown/src/pages/homePage/HomePage.tsx
@@ -3,6 +3,7 @@ import {Button, DataTable, Text} from "react-native-paper";
 import * as React from 'react';
 import { IStackScreenProps } from "../../library/Stack.ScreenProps";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ScrollView } from "react-native-gesture-handler";
 
 const styles=StyleSheet.create({
     screenContainer: {
@@ -17,7 +18,32 @@ const styles=StyleSheet.create({
         alignSelf: 'center',
         marginTop: 10,
         marginBottom: 20,
-      }
+      },
+    scheduleContainer: {
+        flex: 1,
+        width: "80%",
+        verticalAlign: "top",
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
+    timeContainer: {
+        borderRadius: 20,
+        maxHeight: "85%",
+        overflow: "hidden",
+        marginBottom: "2%",
+    },
+    timeRow: {
+        backgroundColor: "#009FFF"
+    },
+    timeName: {
+        flex: 7,
+    },
+    timeTime: {
+        flex: 7,
+    },
+    timeArrow: {
+        flex: 1,
+    },
 })
 
 const handleEditTask = (nav, data) => {
@@ -41,8 +67,6 @@ const HomePage: React.FunctionComponent<IStackScreenProps> = props =>  {
         } catch (error) {
           console.log(error);
         }
-
-        return [];
       };
 
       getData();
@@ -53,20 +77,24 @@ const HomePage: React.FunctionComponent<IStackScreenProps> = props =>  {
                 variant="displayLarge"
                 style={styles.heading}
                 > WattsDown </Text>
-            <View>
-                {data.map(d =>
-                    <DataTable.Row key={JSON.parse(d[1]).id} onPress={() => handleEditTask(navigation, d[1])}>
-                        <DataTable.Cell>{JSON.parse(d[1]).name}</DataTable.Cell>
-                        <DataTable.Cell>{JSON.parse(d[1]).schedule}</DataTable.Cell>
-                        <DataTable.Cell>{">"}</DataTable.Cell>
-                    </DataTable.Row>
-                )}
+            <View style={styles.scheduleContainer}>
+                <View style={styles.timeContainer}>
+                    <ScrollView>
+                    {data.map(d =>
+                        <DataTable.Row key={JSON.parse(d[1]).id} onPress={() => handleEditTask(navigation, d[1])} style={styles.timeRow}>
+                            <DataTable.Cell style={styles.timeName}>{JSON.parse(d[1]).name}</DataTable.Cell>
+                            <DataTable.Cell style={styles.timeTime}>{JSON.parse(d[1]).schedule}</DataTable.Cell>
+                            <DataTable.Cell style={styles.timeArrow}>{">"}</DataTable.Cell>
+                        </DataTable.Row>
+                    )}
+                    </ScrollView>
+                </View>
+                <Button 
+                    mode="contained" 
+                    onPress={() => navigation.navigate('New Task')}>
+                    Add New
+                </Button>
             </View>
-            <Button 
-                mode="contained" 
-                onPress={() => navigation.navigate('New Task')}>
-                Add New
-            </Button>
          
         </View>
     )
