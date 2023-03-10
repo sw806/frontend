c7bb5f88fa3c4ea146bd5044fad3e767f7f0ccc9
Nick / Tempus <56578594+NickDue@users.noreply.github.com>
2023-03-10 13:28:13 +0100

    theming

---
+++ WattsDown/src/styles/palette.ts
@@ -0,0 +1,23 @@
+export const red = {
+    light: "#f06292",
+    regular: "#d32f2f",
+    dark: "#b71c1c",
+}
+
+export const blue = {
+    light: "#4fc3f7",
+    regular: '#009FFF',
+    dark: "#01579b",
+}
+
+export const neutral = {
+    white: '#FFF',
+    black: '#000',
+    grey: "#607d8b",
+}
+
+export const green = {
+    light: "#8bc34a",
+    regular: "#4caf50",
+    dark: "#7cb342",
+}
