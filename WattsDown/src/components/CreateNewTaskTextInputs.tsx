0ed317e0c064a0921454fc2f648c283ab03a66d8
Tommy Dieu <58150918+Dieuer@users.noreply.github.com>
2023-03-10 13:37:56 +0100

    fix: correct input text logic

--- WattsDown/src/components/CreateNewTaskTextInputs.tsx
+++ WattsDown/src/components/CreateNewTaskTextInputs.tsx
@@ -5,25 +5,51 @@ import {View} from "react-native";
 
 type TIProps = {
   name: string,
-  duration: number,
-  energy: number,
-  power: number,
+  duration: string,
+  energy: string,
+  power: string,
+  durationDisabled: boolean,
+  energyDisabled: boolean,
+  powerDisabled: boolean,
+  inputsFilled: number,
   setName,
   setDuration,
   setEnergy,
   setPower
+  setdurationDisabled,
+  setEnergyDisabled,
+  setPowerDisabled,
+  
 }
 
 const CreateNewTaskInputs = (props: TIProps) => {
+
+    
+    const handleInputChange = (name: string, value: string) => {
+        switch (name) {
+          case 'name':
+            props.setName(value);
+            break;
+          case 'duration':
+            props.setDuration(value);
+            break;
+          case 'energy':
+            props.setEnergy(value);
+            break;
+          case 'power':
+            props.setPower(value);
+            break;
+        }
+      }
 
     return(
         <View style={{marginTop: 20}}>
             <TextInput
             mode="outlined"
+            testID="TaskName"
             label="Task Name"
             placeholder="Task Name"
-            right={<TextInput.Affix text="/100" />}
-            onChangeText={text => props.setName(text)}
+            onChangeText={(text) => handleInputChange('name', text)}
             value={props.name}
             activeUnderlineColor='#009FFF'
             activeOutlineColor='#009FFF'
@@ -33,11 +59,12 @@ const CreateNewTaskInputs = (props: TIProps) => {
 
             <TextInput
             mode="outlined"
-            label="Duration"
-            placeholder="Duration"
-            right={<TextInput.Affix text="/100" />}
-            onChangeText={text => props.setDuration(text)}
-            value={props.duration != null ? props.duration.toString() : ''}
+            testID="Duration"
+            label="Duration (minutes)"
+            placeholder="Duration (minutes)"
+            onChangeText={(text) => handleInputChange('duration', text)}
+            value={props.duration}
+            disabled={props.durationDisabled}
             keyboardType="numeric"
             activeUnderlineColor='#009FFF'
             activeOutlineColor='#009FFF'
@@ -47,11 +74,12 @@ const CreateNewTaskInputs = (props: TIProps) => {
 
             <TextInput
             mode="outlined"
-            label="Energy kWh"
-            placeholder="Energy kWh"
-            right={<TextInput.Affix text="/100" />}
-            onChangeText={text => props.setEnergy(text)}
-            value={props.energy != null ? props.energy.toString() : ''}
+            testID="Energy"
+            label="Energy (kWh)"
+            placeholder="Energy (kWh)"
+            onChangeText={(text) => handleInputChange('energy', text)}
+            value={props.energy}
+            disabled={props.energyDisabled}
             keyboardType="numeric"
             activeUnderlineColor='#009FFF'
             activeOutlineColor='#009FFF'
@@ -61,16 +89,18 @@ const CreateNewTaskInputs = (props: TIProps) => {
 
             <TextInput
             mode="outlined"
-            label="Power kW"
-            placeholder="Power kW"
-            right={<TextInput.Affix text="/100" />}
-            onChangeText={text => props.setPower(text)}
-            value={props.power != null ? props.power.toString() : ''}
+            testID="Power"
+            label="Power (kW)"
+            placeholder="Power (kW)"
+            onChangeText={(text) => handleInputChange('power', text)}
+            value={props.power}
+            disabled={props.powerDisabled}
             keyboardType="numeric"
             activeUnderlineColor='#009FFF'
             activeOutlineColor='#009FFF'
             outlineColor='#009FFF'
             underlineColor='#009FFF'
+
             />
         </View>
     )
--- WattsDown/src/pages/createTask/CreateTask.tsx
+++ WattsDown/src/pages/createTask/CreateTask.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Pressable , StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import uuid from 'react-native-uuid';
@@ -14,10 +14,74 @@ const CreateTask: React.FunctionComponent<IStackScreenProps> = props =>  {
   const {navigation, route, nameProp} = props;
   const [isModalVisible, setModalVisible] = useState(false);
   const [name, setName] = useState<string>('');
-  const [duration, setDuration] = useState<number>();
-  const [energy, setEnergy] = useState<number>();
-  const [power, setPower] = useState<number>();
+  const [duration, setDuration] = useState<string>();
+  const [energy, setEnergy] = useState<string>();
+  const [power, setPower] = useState<string>();
   const [startDate, setStartDate] = useState<number>();
+  const [disabledDuration, setDurationDisabled] = useState<boolean>(false);
+  const [disabledEnergy, setEnergyDisabled] = useState<boolean>(false);
+  const [disabledPower, setPowerDisabled] = useState<boolean>(false);
+  const [inputsFilled, setInputsFilled] = useState<number>(0);
+
+  useEffect(() => {
+  
+    if(!duration && !energy && power && disabledPower){
+      setPowerDisabled(false)
+      setStartDate(null);
+
+    }
+    if(!duration && energy && !power && disabledEnergy){
+      setEnergyDisabled(false)
+      setStartDate(null);
+
+    }
+    if(duration && !energy && !power && disabledDuration){
+      setDurationDisabled(false)
+      setStartDate(null);
+
+    }
+    // ... //
+    if(duration && !energy && power && disabledDuration){
+      setPowerDisabled(false)
+      setStartDate(null);
+    }
+    if(duration && energy && !power && disabledDuration){
+      setPowerDisabled(false)
+      setStartDate(null);
+    }
+    if(!duration && energy && power && disabledEnergy){
+      setEnergyDisabled(false)
+      setStartDate(null);
+    }
+    if(duration && energy && !power && disabledEnergy){
+      setEnergyDisabled(false)
+      setStartDate(null);
+    }
+    if(!duration && energy && power && disabledPower){
+      setPowerDisabled(false)
+      setStartDate(null);
+    }
+    if(duration && !energy && power && !disabledPower){
+      setPowerDisabled(false)
+      setStartDate(null);
+    }
+
+    // two values 1 grey
+    if(duration && energy && !power && disabledDuration){
+      setDurationDisabled(false)
+      setStartDate(null);
+    }
+    if(duration && !energy && power && disabledDuration){
+      setDurationDisabled(false)
+      setStartDate(null);
+    }
+    if(duration && energy && !power && disabledDuration){
+      setDurationDisabled(false)
+      setStartDate(null);
+    }
+
+  }, [duration, energy, power]);
+
 
     /*
   const handleScheduleResult = async () => {
@@ -54,7 +118,7 @@ const CreateTask: React.FunctionComponent<IStackScreenProps> = props =>  {
   };
   */
 
-  const createTask = async () => { 
+  const findStartTime = async () => { 
 
     if (!name) {
       alert('Please enter the task name.');
@@ -68,21 +132,26 @@ const CreateTask: React.FunctionComponent<IStackScreenProps> = props =>  {
       return;
     }
 
-    if (power > 3){
-      alert('Power is too high!');
-      return;
+    if( !duration && energy && power){
+      const newDuration = (parseFloat(energy) * 60) / parseFloat(power)
+      setDuration(newDuration.toString());
+      setDurationDisabled(true);
     }
 
-    if( !energy && power && duration){
-      setEnergy((duration / 60) * power);
+    if( !energy && power && duration){  
+      const newEnergy = (parseFloat(duration) / 60) * parseFloat(power)    
+      setEnergy(newEnergy.toString());
+      setEnergyDisabled(true);
     }
 
-    if( energy && !power && duration){
-      setPower( (energy * 60) / duration);
-    }
-
-    if( energy && power && !duration){
-      setDuration((energy * 60) / power);
+    if( !power && energy && duration){
+      const newPower = (parseFloat(energy) * 60) / parseFloat(duration)
+      if(newPower > 3){
+        alert('Power calculated is too high!');
+        return;
+      } 
+      setPower(newPower.toString());
+      setPowerDisabled(true);
     }
 
     setStartDate(1678206600);
@@ -93,9 +162,9 @@ const CreateTask: React.FunctionComponent<IStackScreenProps> = props =>  {
     const newTask: Task = {
       id: uuid.v4().toString(),
       name: name,
-      duration: duration,
-      energy: energy,
-      power: power,
+      duration: parseFloat(duration),
+      energy: parseFloat(energy),
+      power: parseFloat(power),
       startDate: startDate,
     };
   
@@ -175,10 +244,17 @@ const CreateTask: React.FunctionComponent<IStackScreenProps> = props =>  {
         duration={duration}
         energy={energy}
         power={power}
+        durationDisabled={disabledDuration}
+        energyDisabled={disabledEnergy}
+        powerDisabled={disabledPower}
+        inputsFilled={inputsFilled}
         setName={setName}
         setDuration={setDuration}
         setEnergy={setEnergy}
         setPower={setPower}
+        setdurationDisabled={setDurationDisabled}
+        setEnergyDisabled={setEnergyDisabled}
+        setPowerDisabled={setPowerDisabled}
       />
       
       <Button 
@@ -186,10 +262,10 @@ const CreateTask: React.FunctionComponent<IStackScreenProps> = props =>  {
       style={styles.button}
       buttonColor='#009FFF'
       onPress={() => {
-        createTask();
+        findStartTime();
       }}
       >
-        Schedule task
+        Find start time
       </Button>
 
       <ResultArea time={startDate} />
