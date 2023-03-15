8a8c772ba9baa133ed8c094dff285e7ab0c0878e
Lasse Beck <lassebeck@gmail.com>
2023-03-15 14:22:21 +0100

    Chore: Added failing tests for calculations

--- __tests__/calculations.test.tsx
+++ __tests__/calculations.test.tsx
@@ -23,4 +23,31 @@ describe('calculateEnergy', () => {
     expect(calculateEnergy(3.5, 2, 4)).toEqual(0.1167);
     expect(calculateEnergy(4, 2.7, 2)).toEqual(0.18);
   });
+
+describe('calculateDuration', () => {
+  it('should check that the duration calculation fails, when given incorrect power and energy', () => {
+    expect(calculateDuration(1, 0.5)).not.toEqual(40);
+    expect(calculateDuration(2, 2)).not.toEqual(40);
+    expect(calculateDuration(3, 1.5)).not.toEqual(40);
+  });
+
+});
+
+describe('calculatePower', () => {
+  it('should check that the power calculation fails, when given incorrect duration and energy', () => {
+    expect(calculatePower(1.2, 2)).not.toEqual(1);
+    expect(calculatePower(1, 1)).not.toEqual(1.1);
+    expect(calculatePower(1, 0.75)).not.toEqual(1);
+    expect(calculatePower(0.75, 0.75)).not.toEqual(1.2);
+  });
+});
+
+describe('calculateEnergy', () => {
+  it('should check that the energy calculation fails, when given incorrect duration and power', () => {
+    expect(calculateEnergy(3, 1.5, 3)).not.toEqual(1);
+    expect(calculateEnergy(3.5, 2, 4)).not.toEqual(1);
+    expect(calculateEnergy(4, 2.7, 2)).not.toEqual(1);
+  });
+});
+
 });
