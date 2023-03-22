923da9c7ef24793aa6c02e005a2fc15e91b0054e
Nicholas <n.duerlund@gmail.com>
2023-03-22 14:01:00 +0100

    Make overview page

--- src/pages/overview/OverviewPage.tsx
+++ src/pages/overview/OverviewPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {FC} from "react";
-import {StyleSheet, View} from "react-native";
-import {Button, Card, Text} from "react-native-paper";
+import {Dimensions, StyleSheet, View} from "react-native";
+import {Avatar, Button, Card, Text} from "react-native-paper";
 import {components, typography} from "../../styles/theme";
 import {IStackScreenProps} from "../../library/Stack.ScreenProps";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
@@ -28,7 +28,34 @@ const styles = StyleSheet.create({
     },
     infoContainer: {
         flexDirection: "row",
-    }
+    },
+    text: {
+        margin: 2,
+        color: '#000',
+        fontSize: typography.fontSizes.titleLarge.fontSize,
+    },
+    container: {
+        width: Dimensions.get('window').width*0.65,
+        margin: 4
+    },
+    icon: {
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+    },
+    content: {
+        ...components.containers.results,
+    },
+    btn: {
+        ...components.buttons.unstyled.contained,
+    },
+    dualContainer: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+    },
+    editButtons: {
+        position: 'absolute',
+        bottom: 5,
+        width: '100%',
+    },
 })
 
 type OverviewProps = {
@@ -39,7 +66,6 @@ type OverviewProps = {
 
 const OverviewPage: FC = (props:OverviewProps) => {
     const { navigation, route } = props;
-    // @ts-ignore
     const { id, name, duration, energy, power, startDate } = route.params.data;
     return (
         <View style={styles.screenContainer}>
@@ -50,38 +76,75 @@ const OverviewPage: FC = (props:OverviewProps) => {
             <Card style={styles.cardContainer} mode="elevated">
                 <View>
                     <Card.Content>
-                        <Text style={{marginLeft: "25%"}} variant="titleLarge">Task Overview</Text>
+                        <View style={{alignItems: "center"}}>
+                            <Text variant="titleLarge">Task Overview</Text>
+                            <Card style={styles.container} mode="contained">
+                                <Card.Content style={styles.content}>
+                                    <Avatar.Icon
+                                        size={70}
+                                        icon="clock-outline"
+                                        style={styles.icon}
+                                    />
+                                    <Text style={styles.text} variant="bodyMedium">cool</Text>
+                                </Card.Content>
+                            </Card>
+                        </View>
                     </Card.Content>
                 </View>
                 <View style={styles.infoContainer}>
                     <View>
                         <OverviewInfoContainer
-                            icon="clock-outline"
-                            text="somethingMore"
+                            icon="lightning-bolt"
+                            text="Energy"
                         />
                         <OverviewInfoContainer
-                            icon="clock-outline"
-                            text="somethingMore"
+                            icon="currency-eur"
+                            text="Some price"
                         />
                     </View>
                     <View>
                         <OverviewInfoContainer
-                            icon="clock-outline"
-                            text="somethingMore"
+                            icon="power-socket-it"
+                            text="Power"
                         />
                         <OverviewInfoContainer
-                            icon="clock-outline"
-                            text="somethingMore"
+                            icon="molecule-co2"
+                            text="CO2"
                         />
                     </View>
-
                 </View>
-
             </Card>
+            <View style={styles.editButtons}>
+                <View style={styles.dualContainer}>
+                    <Button
+                        style={styles.btn}
+                        mode="elevated"
+                        buttonColor="#607d8b"
+                        textColor="#ffffff"
+                        onPress={() => navigateBack(navigation)}
+                    >
+                        Back
+                    </Button>
+                    <Button
+                        style={styles.btn}
+                        mode="elevated"
+                        buttonColor="#4caf50"
+                        textColor="#ffffff"
+                        onPress={() => navigateEdit(navigation, route.params.data)}
+                    >
+                        Edit
+                    </Button>
+                </View>
+            </View>
         </View>
-
-
     )
+}
+
+const navigateEdit = (nav, data) => {
+    nav.jumpTo('Edit Task', { data: data });
+}
+const navigateBack = (nav) => {
+    nav.jumpTo('Home');
 }
 
 export default OverviewPage
