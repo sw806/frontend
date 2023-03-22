8bdc83d80ef2518192489b94cdb00e4fef15a493
Nick / Tempus <56578594+NickDue@users.noreply.github.com>
2023-03-22 12:39:29 +0100

    OverviewPage

--- src/pages/overview/OverviewPage.tsx
+++ src/pages/overview/OverviewPage.tsx
@@ -1,27 +1,85 @@
 import React from "react";
 import {FC} from "react";
 import {StyleSheet, View} from "react-native";
-import {Text} from "react-native-paper";
-import {components} from "../../styles/theme";
+import {Button, Card, Text} from "react-native-paper";
+import {components, typography} from "../../styles/theme";
+import {IStackScreenProps} from "../../library/Stack.ScreenProps";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {ParamListBase, RouteProp} from "@react-navigation/native";
+import OverviewInfoContainer from "../../components/OverviewInfoContainer";
 
-type OverviewProps = {
-
-}
 
 const styles = StyleSheet.create({
     screenContainer: {
         ...components.containers.screen,
     },
+    heading: {
+        alignSelf: 'center',
+        ...typography.pageHeader.medium,
+    },
+    cardContainer: {
+        display: "flex",
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignSelf: "center",
+        paddingTop: 4,
+        paddingBottom: 4
+    },
+    infoContainer: {
+        flexDirection: "row",
+    }
 })
+
+type OverviewProps = {
+    navigation: NativeStackNavigationProp<any>;
+    route: RouteProp<ParamListBase, any>;
+}
 
 
 const OverviewPage: FC = (props:OverviewProps) => {
+    const { navigation, route } = props;
+    // @ts-ignore
+    const { id, name, duration, energy, power, startDate } = route.params.data;
     return (
         <View style={styles.screenContainer}>
-            <Text>
-                YEs
+            <Text variant="displayLarge" style={styles.heading}>
+                {' '}
+                {name}{' '}
             </Text>
+            <Card style={styles.cardContainer} mode="elevated">
+                <View>
+                    <Card.Content>
+                        <Text style={{marginLeft: "25%"}} variant="titleLarge">Task Overview</Text>
+                    </Card.Content>
+                </View>
+                <View style={styles.infoContainer}>
+                    <View>
+                        <OverviewInfoContainer
+                            icon="clock-outline"
+                            text="somethingMore"
+                        />
+                        <OverviewInfoContainer
+                            icon="clock-outline"
+                            text="somethingMore"
+                        />
+                    </View>
+                    <View>
+                        <OverviewInfoContainer
+                            icon="clock-outline"
+                            text="somethingMore"
+                        />
+                        <OverviewInfoContainer
+                            icon="clock-outline"
+                            text="somethingMore"
+                        />
+                    </View>
+
+                </View>
+
+            </Card>
         </View>
+
 
     )
 }
